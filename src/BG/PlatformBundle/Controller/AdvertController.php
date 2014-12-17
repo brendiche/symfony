91d3d34663a1474ab377fc85ab4c49abacb6911d
brendiche <brendan.gouin92@gmail.com>
2014-12-17 01:16:48 +0100

    la valeur par défault ne marche pas ...

--- src/BG/PlatformBundle/Controller/AdvertController.php
+++ src/BG/PlatformBundle/Controller/AdvertController.php
@@ -4,23 +4,24 @@ namespace BG\PlatformBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 
 class AdvertController extends Controller
 {
-    public function indexAction(){
+    public function indexAction($page){
 
-    	$content = $this->get('templating')->render('BGPlatformBundle:Advert:index.html.twig',array('nom'=>'Brendan'));
-        return new Response($content);
+        // if($page < 1){
+        //     throw new NotFoundException("Page ".$page." inexistante.");
+        // }
+        return $this->render("BGPlatformBundle:Advert:index.html.twig",array("page"=>$page));
     }
     public function index01Action(){
-    	$url = $this->get('router')->generate('bg_platform_view',array("id"=>10),true);
-    	$content = $this->get('templating')->render('BGPlatformBundle:Advert:test.html.twig',array("advert_id"=>10));
-    	// return new Response($url);
-    	return new Response($content);
+    	$url = $this->get('router')->generate('bg_platform_view',array("id"=>10));
+    	return $this->redirect($url);
     }
     public function viewAction($id){
 
-    	return new Response("Affichage de l'annonce avec l'id : ".$id);
+    	return $this->render('BGPlatformBundle:Advert:view.html.twig',array('id'=>$id));
     }
     public function viewSlugAction($year,$slug,$_format){
 
@@ -29,4 +30,19 @@ class AdvertController extends Controller
             slug '".$slug."', créée en ".$year." ,au format ".$_format
         );
     }
+  
+  public function addAction(Request $request)
+  {
+   $session = $request->getSession();
+    
+    // Bien sûr, cette méthode devra réellement ajouter l'annonce
+    
+    // Mais faisons comme si c'était le cas
+    $session->getFlashBag()->add('info', 'Annonce bien enregistrée');
+    // // Le « flashBag » est ce qui contient les messages flash dans la session
+    // // Il peut bien sûr contenir plusieurs messages :
+    $session->getFlashBag()->add('info', 'Oui oui, il est bien enregistré !');
+    // Puis on redirige vers la page de visualisation de cette annonce
+    return $this->redirect($this->generateUrl('bg_platform_view', array('id' => 5)));
+  }
 }
