ab00e62a561424a1c7da2f9fb59707e5cf445e38
brendiche <brendan.gouin92@gmail.com>
2014-12-17 18:50:29 +0100

    first layout

--- src/BG/PlatformBundle/Controller/AdvertController.php
+++ src/BG/PlatformBundle/Controller/AdvertController.php
@@ -10,19 +10,22 @@ class AdvertController extends Controller
 {
     public function indexAction($page){
 
-        // if($page < 1){
-        //     throw new NotFoundException("Page ".$page." inexistante.");
-        // }
-        return $this->render("BGPlatformBundle:Advert:index.html.twig",array("page"=>$page));
+        if($page == null||$page <= 0){
+             $page = 1;
+        }
+        return $this->render("BGPlatformBundle:Advert:index.html.twig",array("listAdverts"=>array()));
     }
+
     public function index01Action(){
     	$url = $this->get('router')->generate('bg_platform_view',array("id"=>10));
     	return $this->redirect($url);
     }
+
     public function viewAction($id){
 
     	return $this->render('BGPlatformBundle:Advert:view.html.twig',array('id'=>$id));
     }
+
     public function viewSlugAction($year,$slug,$_format){
 
     	  return new Response(
@@ -31,18 +34,33 @@ class AdvertController extends Controller
         );
     }
   
-  public function addAction(Request $request)
-  {
-   $session = $request->getSession();
-    
-    // Bien sûr, cette méthode devra réellement ajouter l'annonce
-    
-    // Mais faisons comme si c'était le cas
-    $session->getFlashBag()->add('info', 'Annonce bien enregistrée');
-    // // Le « flashBag » est ce qui contient les messages flash dans la session
-    // // Il peut bien sûr contenir plusieurs messages :
-    $session->getFlashBag()->add('info', 'Oui oui, il est bien enregistré !');
-    // Puis on redirige vers la page de visualisation de cette annonce
-    return $this->redirect($this->generateUrl('bg_platform_view', array('id' => 5)));
-  }
+    public function addAction($id,Request $request){
+        if($request->isMethod('POST')){
+            $request->getSession()->getFlashBag()->add('notice','Annonce bien enregistrée.');
+            return $this->redirect($this->generateUrl('bg_platform_view', array('id' => 5)));    
+        }
+        return $this->render('BGPlatformBundle:Advert:add.html.twig');
+    }
+
+    public function editAction(Request $request){
+        if($request->isMethod('POST')){
+            $request->getSession()->getFlashBag()->add('notice','Annonce bien modifiée.');
+            return $this->redirect($this->generateUrl('bg_platform_view', array('id' => 5)));    
+        }
+        return $this->render('BGPlatformBundle:Advert:edit.html.twig');
+    }
+
+    public function deleteAction($id){
+        return $this->render('BGPlatformBundle:Advert:delete.html.twig');
+    }
+
+    public function menuAction(){
+        $listAdverts = array(
+          array('id' => 2, 'title' => 'Recherche développeur Symfony2'),
+          array('id' => 5, 'title' => 'Mission de webmaster'),
+          array('id' => 9, 'title' => 'Offre de stage webdesigner')
+        );
+
+        return $this->render('BGPlatformBundle:Advert:menu.html.twig', array('listAdverts' => $listAdverts));
+    }
 }
